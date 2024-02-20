797fd7ade8a481b9683b692ccefc862a02fdb62d
eduardooliveira <eos.eduardooliveira@gmail.com>
2024-02-20 10:50:18 -0300

    Added Suspense

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,14 @@
 import { AddTag } from "@/components/add-tag";
 import { Tags } from "@/components/tags";
+import { Suspense } from "react";
 
 export default function Home() {
   return (
     <main>
       <div>
-        <Tags />
+        <Suspense fallback={<p>Carregando Tags...</p>}>
+          <Tags />
+        </Suspense>
         <AddTag />
       </div>
     </main>
--- src/components/tags.tsx
+++ src/components/tags.tsx
@@ -1,7 +1,10 @@
 export async function Tags() {
+  
+  await new Promise(resolve => setTimeout(resolve, 3000))
+
   const response = await fetch('http://localhost:3333/tags', {
     next: {
-      tags: ['tags']
+      tags: ['get-tags']
     }
   })
   const data = await response.json()
